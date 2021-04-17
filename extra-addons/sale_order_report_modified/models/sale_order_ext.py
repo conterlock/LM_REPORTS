ae65aceb321a1d312d3058a39942285eb9ae7f1a
Luis Millan <lmillan131@gmail.com>
2021-04-17 11:48:59 -0400

    correcciones

--- extra-addons/sale_order_report_modified/models/sale_order_ext.py
+++ extra-addons/sale_order_report_modified/models/sale_order_ext.py
@@ -19,7 +19,7 @@ class SaleOrderExt(models.Model):
         else:
             num = 19
 
-        return list(range(num-1.5*len(self.order_line)-len(banks)))
+        return list(range(num-int(1.5*len(self.order_line))-len(banks)))
 
     def convert_date_letters(self):
         """this method transforms the date to letters"""
